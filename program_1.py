3f8f418cdaf9fcaff1acd988b7e747efddc41437
MakenzieTotushek <mltotushek@students.unwsp.edu>
2026-02-27 14:43:57 -0600

    Modified Programs Week 6

--- program_1.py
+++ program_1.py
@@ -1,15 +1,29 @@
+# Author: Makenzie Totushek
+# Date: 2/27/26
+# Title: Random Dice
 
 # Program #1: Random Dice
 # Write a "randDice" function (with no input) that randomly chooses two numbers between 1 and 6 (inclusive) and then adds them (this is to simulate the rolling of 2 dice).  
 # The dice sum will be the output of this function.
+import random
+print('Rolling the dice...')
 
 def randDice():
     # Write your logic to generate 2 numbers between 1 and 6 here
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
 
     # Sum 2 numbers
-
+    sum = dice1 + dice2
     # return sum to calling function
-
+    return sum
 #########
 # Then write a mainline that calls the "randDice" function 100 times in a for loop.  
+
+total = 0
+for n in range(100):
+    dice_rolls = randDice()
+    total = total + dice_rolls
 # The mainline then prints the average of the 100 rolls, rounded to the nearest 0.01.
+average = total / 100
+print(f'The total average of your dice rolls is {average:.2f}')
--- program_3.py
+++ program_3.py
@@ -1,3 +1,7 @@
+# Author: Makenzie Totushek
+# Date: 2/27/26
+# Title: Tax Rate
+
 # Program #3: Tax Rate
 # A retail company must file a monthly sales tax report listing the total sales for the month, 
 # and the amount of state and county sales tax collected. 
@@ -9,3 +13,15 @@
 # The amount of state sales tax.
 # The total sales tax (county plus state)
 # Use at least one function with input and output in this program
+
+total_sales = float(input("Enter the total amount for sales: "))
+def cTax():
+    county_tax = total_sales * 0.025
+    return county_tax
+def sTax():
+    state_tax = total_sales * 0.05
+    return state_tax
+total_tax = cTax() + sTax()
+print(f'The total county tax is ${cTax():.2f}')
+print(f'The total state tax is ${sTax():.2f}')
+print(f'The total sales tax is ${total_tax:.2f}')
